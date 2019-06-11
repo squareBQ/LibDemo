a7e06d9856013c2407c96749003131196f370288
zahileoo <zahileoo@gmail.com>
2019-06-11 13:16:09 +0800

    add SparseArray usage

--- demo/src/main/java/com/zahi/one/libdemo/MainActivity.java
+++ demo/src/main/java/com/zahi/one/libdemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.zahi.one.libdemo;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
 import android.widget.TextView;
 
 import com.zahi.lib.zahilib.widget.DrawableBuilder;
@@ -21,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
                 .corner(0)
                 .build();
         tv.setBackground(drawable);
+
+        SparseArray<String> array = new SparseArray<>(2);
+        String arr0 = array.get(0);
+        Log.e("MainActivity", "0 is: " + arr0 + ", 1 is: " + array.get(1));
+        arr0 = "Zero";
+        array.put(1, "One");
+        Log.e("MainActivity", "0 is: " + array.get(0) + ", 1 is: " + array.get(1));
     }
 }
